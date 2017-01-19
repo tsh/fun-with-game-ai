cfe175a47eb605f09aa005a9e9f00c67eb22930f
tsh <dr.tallin@gmail.com>
2017-01-19 20:30:56 +0200

    draft seek

--- steering_and_locomotion/seek.py
+++ steering_and_locomotion/seek.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.clock import Clock
 from kivy.uix.widget import Widget
 from kivy.graphics import Color, Ellipse, Rectangle
+from kivy.vector import Vector
 
 from vehicle import Vehicle
 
@@ -10,11 +11,16 @@ class SeekWidget(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.seeker = Vehicle()
+        self.target = Vector(400, 100)
 
     def on_touch_down(self, touch):
         self.canvas.add(Rectangle(size=(50, 50)))
 
     def update(self, dt):
+        self.canvas.add(Ellipse(pos=(self.target), size=(10, 10)))
+        desired = (self.target - self.seeker.location).normalize() * self.seeker.max_speed
+        steer = desired - self.seeker.velocity
+        self.seeker.acceleration += steer
         self.seeker.update(dt)
         self.seeker.render(self.canvas)
 
--- steering_and_locomotion/vehicle.py
+++ steering_and_locomotion/vehicle.py
@@ -3,11 +3,16 @@ from kivy.graphics import Canvas, Rectangle
 
 class Vehicle():
     def __init__(self):
-        self.position = Vector(5, 15)
+        self.location = Vector(5, 15)
+        self.velocity = Vector(0, 0)
+        self.acceleration = Vector(0, 0)
+        self.max_speed = 10
+        self.max_force = 0.1
 
 
     def update(self, dt):
-        self.position += dt
+        self.velocity += self.acceleration
+        self.location += self.velocity * dt
 
     def render(self, canvas: Canvas):
-        canvas.add(Rectangle(pos=self.position, size=(50, 50)))
+        canvas.add(Rectangle(pos=self.location, size=(50, 50)))
